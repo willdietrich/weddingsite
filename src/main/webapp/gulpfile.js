77cb3dd21ea91bb249c5d9a998127f73a689d140
Will Dietrich <will.a.dietrich@gmail.com>
2018-09-25 00:35:34 -0500

    Add basic scss sheets.

---
+++ src/main/webapp/gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let path = require('path');
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+let basedir = "/home/will/dev/weddingsite/";
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(path.join(basedir, "target/classes/static")));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/*.scss', ['sass']);
+});
