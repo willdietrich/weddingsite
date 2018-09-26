7616ab926fdd728ec45031114b5a525b9aec5ed4
Will Dietrich <will.a.dietrich@gmail.com>
2018-09-25 23:04:14 -0500

    Add minification for css.

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -4,12 +4,17 @@ let path = require('path');
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 
 let basedir = "../../..";
 
 gulp.task('sass', function () {
     return gulp.src('./scss/main.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.join(basedir, 'target/', '/classes', '/static', '/style')));
 });
 
